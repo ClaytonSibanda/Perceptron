e3a64c44b79599951ab83dcfdaa59222c97ca08c
Clayton <sibandaclayton0@gmail.com>
2018-08-25 01:07:29 +0200

    added perceptron class

---
+++ CLionProjects/Perceptron/perceptron.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nyasha on 2018/08/25.
+//
+
+#include "perceptron.h"
---
+++ CLionProjects/Perceptron/perceptron.h
@@ -0,0 +1,20 @@
+//
+// Created by Nyasha on 2018/08/25.
+//
+
+#ifndef PERCEPTRON_PERCEPTRON_H
+#define PERCEPTRON_PERCEPTRON_H
+
+
+class perceptron {
+    
+    
+    
+    
+    
+    
+
+};
+
+
+#endif //PERCEPTRON_PERCEPTRON_H
